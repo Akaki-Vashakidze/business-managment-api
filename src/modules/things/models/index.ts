5e8825b74b8b7c2b6c36c75c84cf3ea3e5a95173
Ako <akaki.vashakidze1@gmail.com>
2026-01-21 14:46:18 +0400

    remove schedule schema

--- src/modules/things/models/index.ts
+++ src/modules/things/models/index.ts
@@ -1,12 +1,10 @@
 
 import { AccessToken, AccessTokenSchema } from "./access-token.schema";
 import { EmailVerification, EmailVerificationSchema } from "./email-verification.schema";
-import { Schedule, ScheduleSchema } from "./schedule.schema";
 import { User, UserSchema } from "./user.schema";
 
 const MongooseModels = [
   { name: User.name, schema: UserSchema },
-  { name: Schedule.name, schema: ScheduleSchema },
   { name: AccessToken.name, schema: AccessTokenSchema },
   { name: EmailVerification.name, schema: EmailVerificationSchema },
 ];
--- src/modules/things/things.module.ts
+++ src/modules/things/things.module.ts
@@ -3,10 +3,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import MongooseModels from './models';import * as dotenv from 'dotenv'; 
 import { AuthController } from './controllers/auth.controller';
-import { ScheduleController } from './controllers/schedule.controller';
 import { AuthService } from './services/auth.service';
 import { MailService } from './services/mail.service';
-import { ScheduleService } from './services/schedule.service';
 import { JwtTokenService } from './services/jwt-token.service';
 
 dotenv.config();
@@ -16,8 +14,8 @@ dotenv.config();
         MongooseModule.forRoot(process.env.DB_URL!),
         MongooseModule.forFeature(MongooseModels),
     ],
-    controllers: [AuthController, ScheduleController],
-    providers: [AuthService, MailService, ScheduleService,JwtTokenService],
+    controllers: [AuthController],
+    providers: [AuthService, MailService, JwtTokenService],
 })
 export class ThingsModule {
     constructor() {
