8daad23dc401ed524d5ce0b744ad93c31d7bc4cd
Ako <akaki.vashakidze1@gmail.com>
2026-02-12 09:42:48 +0400

    fx

--- src/modules/things/dtos/signup.dto.ts
+++ src/modules/things/dtos/signup.dto.ts
@@ -4,9 +4,6 @@ export class SignupDto  {
 
     @IsString()
     @MinLength(6)
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, {
-        message: 'Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, and one number.'
-    })
     password: string;
 
     @IsOptional()
